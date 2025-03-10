84236129c912f43dafa53d2b81d20b4ecd348772
nagendraalluri <nagendra0353@gmail.com>
2025-03-10 17:34:14 -0400

    cypress scripts for for-automation-exercise-website

---
+++ cypress/e2e/firsttest.cy.js
@@ -0,0 +1,5 @@
+describe('Launch application', () => {
+    it('Launch the amazon application', () => {
+      cy.visit('https://www.amazon.com/')
+    })
+  })
---
+++ cypress/e2e/re_registration.cy.js
@@ -0,0 +1,21 @@
+describe('Registration Functionality Error', () => {
+    it('Re-Registration User with Same Email - Error Validation', () => {
+        cy.visit('https://automationexercise.com/');
+        cy.get('.shop-menu .nav li:nth-child(4) a').click();
+        cy.get('[data-qa="signup-name"]').type('nagendra');
+        cy.get('[data-qa="signup-email"]').type('nagendra8019@gmail.com');
+        cy.get('[data-qa="signup-button"]').click();
+
+        // Check if "Email Address already exist!" message appears
+        cy.get('body').then(($body) => {
+            if ($body.find('.signup-form > form > p:contains("Email Address already exist!")').length > 0) {
+                cy.log('❌ Email already exists! Stopping test execution.');
+                return; // Stops further execution
+            } else {
+                // Continue only if email does not exist
+                cy.get('#id_gender1').should('be.visible').check();
+                // Add further form-filling steps here
+            }
+        });
+    });
+});
